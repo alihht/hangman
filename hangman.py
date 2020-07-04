493c0aee23e368e99a73ccb1fc1b342bdc6ea06d
Ali Altalag <alihht@hotmail.com>
2020-07-04 13:50:47 +0300

    able to find multi letters

--- hangman.py
+++ hangman.py
@@ -1,7 +1,8 @@
-word = 'string'
+word = 'strings'
 word_arr=[]
 dash_word_array=[]
-count=0
+trys = len(word)+5
+print("you have " + str(trys))
 global u_input
 
 def load_to_array():
@@ -9,21 +10,47 @@ def load_to_array():
         word_arr.append(i)
 
 def clone_arr():
-    for i in range(len(word_arr)):
+    for _ in range(len(word_arr)):
         dash_word_array.append('_')
 
 def user_input():
+    global trys
     global u_input
-    for i in range(len(word)):
+    for _ in range(trys):
         u_input=input()
         check_input()
 
 def check_input():
+    global trys
     global u_input
-    for i, j in zip(word_arr, range(len(word_arr))):
-        if(u_input == i):
-            dash_word_array[j] = i
+    k=[]
+    #for i, j in zip(word_arr, range(len(word_arr))):
+    if(u_input in word_arr):
+        for n in range(len(word_arr)):
+            if(word_arr[n] == u_input):
+                k.append(word_arr.index(u_input, n, len(word)))
+        print(k)
+        for j in k:
+            print(j)
+            #j = word_arr.index(u_input)
+            dash_word_array[j] = u_input
             print(dash_word_array)
+            print("you have " + str(trys))
+            #break
+    else:
+        check_input2()
+            #break
+
+def check_input2():
+    global trys
+    global u_input
+    for i in word_arr:
+        if(u_input != i):
+            print("Im in else")
+            trys = trys - 1
+            print("you have " + str(trys))
+            #break
+
 
 load_to_array()
 clone_arr()
